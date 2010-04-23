fab5a0813fad5369db3d6d0e12ee435011ae8cc5
flier.lu <flier.lu@26d1db24-2978-dc0d-8aaa-4fe2372774f8>
2010-04-23 10:25:44 +0000

    add urllib style interface

---
+++ trunk/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'urllib4',
+    version = '0.1',
+    packages = find_packages(exclude=['ez_setup']),
+    author = 'Flier Lu',
+    author_email = 'flier.lu@gmail.com',
+    description = 'urllib style HTTP/FTP client library base on pycurl',
+    long_description = open('README.txt').read(),
+    license = 'Apache License 2.0',
+    keywords = 'urllib pycurl network http ftp',
+    url = 'http://code.google.com/p/urllib4/',
+    download_url = 'http://code.google.com/p/urllib4/downloads/list',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',        
+    ]
+)
