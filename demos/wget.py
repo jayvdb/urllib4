f83d95697423c6d4f757b1c8c1b62071d0b98920
flier.lu <flier.lu@26d1db24-2978-dc0d-8aaa-4fe2372774f8>
2010-06-09 03:17:38 +0000

    support to dump the downloading progress

--- demos/wget.py
+++ demos/wget.py
@@ -2,7 +2,7 @@
 from __future__ import with_statement
 
 import sys, os, os.path
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 
 try:
@@ -14,6 +14,8 @@ import urllib4
 
 DEFAULT_LOG_LEVEL = logging.WARN
 DEFAULT_LOG_FORMAT = '%(asctime)s %(levelname)s [%(process)d:%(thread)d] %(module)s.%(name)s: %(message)s'
+
+PROGRESS_BAR_LENGTH = 40
 
 def parse_cmdline():
     from optparse import OptionParser
@@ -54,6 +56,37 @@ def size_pretty(size):
         return "%.1fK" % (size / KB)
     
     return "%dB" % int(size)
+    
+def timedelta_prettry(delta):
+    MINUTE = 60
+    HOUR = MINUTE * 60    
+    
+    s = ""
+    
+    if delta.days > 0:
+        s += '%d d' % delta.days
+        
+    if delta.seconds > HOUR:
+        s += '%d h' % (delta.seconds / HOUR)
+        
+    if (delta.seconds % HOUR) > MINUTE:
+        s += '%d h' % (delta.seconds / MINUTE)
+        
+    s += '%d s' % (delta.seconds % MINUTE)
+    
+    return s
+        
+def update_status(str):
+    if sys.platform == 'win32':
+        import win32console 
+        
+        buf = win32console.GetStdHandle(win32console.STD_OUTPUT_HANDLE)
+        pos = buf.GetConsoleScreenBufferInfo()['CursorPosition']
+        size = buf.GetConsoleScreenBufferInfo()['MaximumWindowSize']        
+        buf.SetConsoleCursorPosition(win32console.PyCOORDType(0, pos.Y))
+        buf.WriteConsole(str+' '*(size.X-len(str)-1))
+    else:
+        print str
 
 if __name__ == '__main__':
     opts, args = parse_cmdline()
@@ -69,16 +102,37 @@ if __name__ == '__main__':
         
         print "HTTP request sent, awaiting response...",
         
-        download_progress = []
+        download_records = []
                 
         def onprogress(download_total, downloaded, upload_total, uploaded):
-            if len(download_progress) == 0:
-                print c.status
-                print "Length: %d (%s) [%s]" % (int(download_total), size_pretty(download_total), c.headers.get('Content-Type'))
+            if download_total > 0:            
+                if len(download_records) == 0:
+                    print c.status
+                    print "Length: %d (%s) [%s]" % (int(download_total), size_pretty(download_total), c.headers.get('Content-Type'))
+                    
+                    print "Saving to: `%s`" % filename
+                    print
                 
-                print "Saving to: `%s`" % filename
+                download_records.append([datetime.now(), downloaded])
                 
-            download_progress.append([datetime.now(), downloaded])
+                download_percent = downloaded/download_total if download_total else 0
+                download_progress = '='*int(download_percent * PROGRESS_BAR_LENGTH)
+                
+                if len(download_progress) < PROGRESS_BAR_LENGTH:
+                    download_progress += '>'
+                    download_progress += ' '*(PROGRESS_BAR_LENGTH-len(download_progress))
+                
+                download_status = "%3d%% [%s] %-10d" % (download_percent*100, download_progress, downloaded)
+                
+                download_time = (datetime.now() - download_records[0][0]).seconds
+                
+                if download_time > 0:
+                    download_speed = downloaded/download_time
+                    download_expect = timedelta(seconds=(download_total - downloaded) / download_speed)
+                    
+                    download_status += " %s/s eta %s" % (size_pretty(download_speed), timedelta_prettry(download_expect))
+                
+                update_status(download_status)
                 
             return urllib4.PROGRESS_CALLBACK_CONTINUE
                     
