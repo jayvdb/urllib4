c5e1790fa2ff086f64463617d82b113965e4cbdd
flier.lu <flier.lu@26d1db24-2978-dc0d-8aaa-4fe2372774f8>
2010-06-18 09:01:11 +0000

    add urlretrieve for urllib

--- trunk/urllib4/__init__.py
+++ trunk/urllib4/__init__.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+import os
+from tempfile import mkstemp
+
 from request import HttpRequest, REDIRECT_INFINITE, REDIRECT_REFUSE
 from response import HttpResponse
 from errors import *
@@ -21,8 +24,7 @@ Request = HttpRequest
 
 def urlopen(url_or_request, data_or_reader=None, headers={}, method=None,
             guess_encoding=None, dnscache=None, pagecache=None, progress_callback=None,
-            session_timeout=None, connect_timeout=None,
-            *args, **kwds):
+            session_timeout=None, connect_timeout=None, *args, **kwds):
 
     if issubclass(type(url_or_request), HttpRequest):
         request = url_or_request
@@ -38,3 +40,22 @@ def urlopen(url_or_request, data_or_reader=None, headers={}, method=None,
 
     return HttpClient(dnscache=dnscache, pagecache=pagecache,
         profile=profile, guess_encoding=guess_encoding).perform(request, progress_callback)
+
+def urlretrieve(url, filename=None, reporthook=None, data_or_reader=None,
+                dnscache=None, pagecache=None, *args, **kwds):
+    
+    request = HttpRequest(url, data_or_reader, **kwds)
+    client = HttpClient(dnscache=dnscache, pagecache=pagecache)
+
+    if filename:
+        client.file = open(filename)
+    else:
+        fd, filename = mkstemp()
+        client.file = os.fdopen(fd)
+
+    def progress(download_total, downloaded, upload_total, uploaded):
+        reporthook(downloaded, 1, download_total)
+
+    response = client.perform(request, progress if reporthook else None)
+
+    return (filename, response.headers)
