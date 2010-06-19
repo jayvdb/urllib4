cc2c3384f19787285c9dd51408fa4d001b7b7c15
flier.lu <flier.lu@26d1db24-2978-dc0d-8aaa-4fe2372774f8>
2010-06-19 17:37:18 +0000

    extract urllib2 related functions to adapter.py add required libraries to setup.py

--- urllib4/__init__.py
+++ urllib4/__init__.py
@@ -10,7 +10,11 @@ from dnscache import DnsCache
 from pagecache import DictPageCache, MemcachePageCache
 from flowcontrol import SiteProfile
 from connpool import BaseConnection, ConnectionPool
+from adapter import Request, urlopen, urlretrieve
 
+__version__ = '0.3'
+__author__ = 'Flier Lu <flier.lu@gmail.com>'
+__url__ = 'http://code.google.com/p/urllib4/'
 __all__ = ['HttpRequest', 'REDIRECT_INFINITE', 'REDIRECT_REFUSE',
            'HttpResponse', 'HttpClient',
            'PROGRESS_CALLBACK_CONTINUE', 'PROGRESS_CALLBACK_ABORT',
@@ -19,43 +23,3 @@ __all__ = ['HttpRequest', 'REDIRECT_INFINITE', 'REDIRECT_REFUSE',
            'ProxyResolveError', 'HostResolveError', 'OperationTimeoutError',
            'BaseConnection', 'ConnectionPool',
            'Request', 'urlopen',]
-
-Request = HttpRequest
-
-def urlopen(url_or_request, data_or_reader=None, headers={}, method=None,
-            guess_encoding=None, dnscache=None, pagecache=None, progress_callback=None,
-            session_timeout=None, connect_timeout=None, *args, **kwds):
-
-    if issubclass(type(url_or_request), HttpRequest):
-        request = url_or_request
-    else:
-        request = HttpRequest(str(url_or_request), data_or_reader, headers, method, *args, **kwds)
-
-    if session_timeout or connect_timeout:
-        profile = SiteProfile.get(request.hostname)
-        profile.timeout = session_timeout
-        profile.connect_timeout = connect_timeout
-    else:
-        profile = None
-
-    return HttpClient(dnscache=dnscache, pagecache=pagecache,
-        profile=profile, guess_encoding=guess_encoding).perform(request, progress_callback)
-
-def urlretrieve(url, filename=None, reporthook=None, data_or_reader=None,
-                dnscache=None, pagecache=None, *args, **kwds):
-
-    request = HttpRequest(url, data_or_reader, *args, **kwds)
-    client = HttpClient(dnscache=dnscache, pagecache=pagecache)
-
-    if filename:
-        client.file = open(filename)
-    else:
-        fd, filename = mkstemp()
-        client.file = os.fdopen(fd)
-
-    def progress(download_total, downloaded, upload_total, uploaded):
-        reporthook(downloaded, 1, download_total)
-
-    response = client.perform(request, progress if reporthook else None)
-
-    return (filename, response.headers)
---
+++ urllib4/adapter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from request import HttpRequest
+from client import HttpClient
+
+Request = HttpRequest
+
+def urlopen(url_or_request, data_or_reader=None, headers={}, method=None,
+            guess_encoding=None, dnscache=None, pagecache=None, progress_callback=None,
+            session_timeout=None, connect_timeout=None, *args, **kwds):
+
+    if issubclass(type(url_or_request), HttpRequest):
+        request = url_or_request
+    else:
+        request = HttpRequest(str(url_or_request), data_or_reader, headers, method, *args, **kwds)
+
+    if session_timeout or connect_timeout:
+        profile = SiteProfile.get(request.hostname)
+        profile.timeout = session_timeout
+        profile.connect_timeout = connect_timeout
+    else:
+        profile = None
+
+    return HttpClient(dnscache=dnscache, pagecache=pagecache,
+        profile=profile, guess_encoding=guess_encoding).perform(request, progress_callback)
+
+def urlretrieve(url, filename=None, reporthook=None, data_or_reader=None,
+                dnscache=None, pagecache=None, *args, **kwds):
+
+    request = HttpRequest(url, data_or_reader, *args, **kwds)
+    client = HttpClient(dnscache=dnscache, pagecache=pagecache)
+
+    if filename:
+        client.file = open(filename)
+    else:
+        fd, filename = mkstemp()
+        client.file = os.fdopen(fd)
+
+    def progress(download_total, downloaded, upload_total, uploaded):
+        reporthook(downloaded, 1, download_total)
+
+    response = client.perform(request, progress if reporthook else None)
+
+    return (filename, response.headers)
